81faad8c9ec41ed9b9833d57acc0851053a6bf5e
AvimanShahi <119503363+AvimanShahi@users.noreply.github.com>
2024-04-12 18:13:56 +0545

    user information is displayed in admin panel

--- users.php
+++ users.php
@@ -21,6 +21,14 @@ echo "<h2>User Details</h2>";
 echo "<table border='1'>";
 echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>Address</th></tr>";
 
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['address'] . "</td>";
+    echo "</tr>";
+}
 
 echo "</table>";
 
